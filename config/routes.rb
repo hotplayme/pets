41daf90194c7dd8ec2ff031dfe8d0ac9c2181d22
play <play@MacBook-Air-alex.local>
2019-05-07 11:01:50 +0700

    add deploy, fix article path

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
-  resources :articles, only: [:index, :show] do
-    get '/articles', to: 'articles#index', as: :articles
-    get '/articles/:id/:slug', to: 'articles#show', as: :article
+  resources :articles, only: [:index] do
     resources :comments, only: [:create]
   end
+  get '/articles/:id/:slug', to: 'articles#show', as: 'article'
 
 
   get '/admin', to: 'admin#index', as: :admin
